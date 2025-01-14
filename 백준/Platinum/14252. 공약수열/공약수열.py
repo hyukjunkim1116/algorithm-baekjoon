ebd0c90cd39512dce14b125ab3cb043d8729d3f0
Hyuk Jun Kim <126032661+hyukjunkim1116@users.noreply.github.com>
2025-01-15 03:27:02 +0900

    [Platinum V] Title: 공약수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/14252. 공약수열/공약수열.py
+++ 백준/Platinum/14252. 공약수열/공약수열.py
@@ -1,6 +1,5 @@
-import sys
-
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 n = int(input())
 
 arr = sorted(list(map(int, input().split())))
@@ -13,19 +12,19 @@ def gcd(a, b):
     b = tmp
   return b
 
-
 count = 0
 arr2 = []
 
 for i in range(len(arr) - 1):
   if gcd(arr[i], arr[i + 1]) > 1:
     arr2.append([arr[i], arr[i + 1]])
+
 for a, b in arr2:
   for j in range(a + 1, b):
     tmp = 0
     if gcd(a, j) == 1: tmp += 1
     if gcd(b, j) == 1: tmp += 1
-    
+
     if tmp > 1:
       count += 1
       break
