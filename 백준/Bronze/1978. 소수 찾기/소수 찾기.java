2316aa0faf7373aee7d53ae34c1d909212436812
Hyuk Jun Kim <126032661+hyukjunkim1116@users.noreply.github.com>
2025-01-14 17:12:17 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int answer = 0;
+    List<Integer> list = new ArrayList<>();
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    while (st.hasMoreTokens()) {
+      list.add(Integer.parseInt(st.nextToken()));
+    }
+
+    for (int i = 0; i < N; i++) {
+      int cand = list.get(i);
+      for (int j = 2; j < cand + 1; j++) {
+        if (j == cand) {
+          answer++;
+          break;
+        }
+        if (cand % j == 0) {
+          break;
+        }
+      }
+    }
+    System.out.println(answer);
+  }
+}
