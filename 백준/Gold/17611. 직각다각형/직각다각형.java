3f45b4b1b3e47074b7a73536a71be71e9dbb2104
Hyuk Jun Kim <126032661+hyukjunkim1116@users.noreply.github.com>
2025-01-16 03:27:14 +0900

    [Gold II] Title: 직각다각형, Time: 176 ms, Memory: 30200 KB -BaekjoonHub

--- 백준/Gold/17611. 직각다각형/직각다각형.java
+++ 백준/Gold/17611. 직각다각형/직각다각형.java
@@ -2,71 +2,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
 
-        int[][] arr = new int[N+1][N+1];
-
-        int[] arrX = new int[N+1];
-        int[] arrY = new int[N+1];
-
+        int[] arrX = new int[N + 1];
+        int[] arrY = new int[N + 1];
 
         int maxX = Integer.MIN_VALUE;
         int maxY = Integer.MIN_VALUE;
         int minX = Integer.MAX_VALUE;
         int minY = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine()," ");
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                int[] tmp = new int[2];
-                tmp[0] = x;
-                tmp[1] = y;
-                arr[i] = tmp;
-                arrX[i] = x;
-                arrY[i] = y;
 
-                if (maxX < x) maxX = x;
-                if (maxY < y) maxY = y;
-                if (minX > x) minX = x;
-                if (minY > y) minY = y;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arrX[i] = x;
+            arrY[i] = y;
+
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
         }
+
+        arrX[N] = arrX[0];
+        arrY[N] = arrY[0];
+
         int diffX = maxX - minX;
         int diffY = maxY - minY;
-        arrX[arrX.length-1] = arrX[0];
-        arrY[arrY.length-1] = arrY[0];
-        System.out.println(Math.max(getMax(N,arrY,minY,diffY),getMax(N,arrX,minX,diffX)));
+
+        int result = Math.max(
+                getMax(N, arrX, minX, diffX),
+                getMax(N, arrY, minY, diffY)
+        );
+
+        System.out.println(result);
     }
-    private static int getMax(int count,int[] array, int minimum, int difference) {
-        int[] graph = new int[difference+1];
-        for(int i = 0; i < count; i++) {
-            if (array[i] != array[i+1] ) {
-                int low = Math.min(array[i], array[i+1]) - minimum;
-                int high = Math.max(array[i], array[i+1]) - minimum;
-                graph[low] += 1;
-                if (high < difference +1) {
-                    graph[high] -= 1;
-                }
+
+    private static int getMax(int N, int[] array, int minimum, int difference) {
+        int[] graph = new int[difference + 1];
+
+        for (int i = 0; i < N; i++) {
+            int low = Math.min(array[i] - minimum, array[i + 1] - minimum);
+            int high = Math.max(array[i] - minimum, array[i + 1] - minimum);
+
+            if (array[i] != array[i + 1]) {
+                graph[low]++;
+                graph[high]--;
             }
         }
 
-        int[] prefix = new int[difference+1];
-        for(int i = 0; i < prefix.length; i++) {
-            if (i == 0) prefix[i] = graph[0];
-            else prefix[i] = 0;
+        int[] prefix = new int[difference + 1];
+        for (int i = 0; i < graph.length; i++) {
+            prefix[i] = (i == 0) ? graph[i] : prefix[i-1] + graph[i];
         }
-        int max = Integer.MIN_VALUE;
-        for(int i = 1; i < difference+1; i++) {
-            prefix[i] = prefix[i-1] + graph[i];
-            if (prefix[i] > max) max = prefix[i];
-        }
-        return max;
+
+        return java.util.Arrays.stream(prefix)
+                .max()
+                .orElse(0);
     }
 }
