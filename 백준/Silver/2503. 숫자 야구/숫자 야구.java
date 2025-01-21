35ffa2a2de55cc490f0c5a5f3b5af6b73e3d56d1
Hyuk Jun Kim <126032661+hyukjunkim1116@users.noreply.github.com>
2025-01-21 19:11:40 +0900

    [Silver III] Title: 숫자 야구, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,58 +1,70 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+  private static int answer = 0;
+  private static int N;
+  private static int[][] arr;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    arr = new int[N][3];
     StringTokenizer st;
-    int N = Integer.parseInt(br.readLine());
-    int answer = 0;
-    List<List<Integer>> hintList = new ArrayList<>(); // {}
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine(), " ");
-      List<Integer> list = new ArrayList<>();
       for (int j = 0; j < 3; j++) {
-        list.add(Integer.parseInt(st.nextToken()));
-      }
-      hintList.add(list);
-    }
-
-    for (int a = 1; a < 10; a++) {
-      for (int b = 1; b < 10; b++) {
-        for (int c = 1; c < 10; c++) {
-          int count = 0;
-          if (a == b | b == c | c == a) continue;
-          for (List<Integer> hint : hintList) {
-            int number = hint.get(0); // 123
-            int number100 = number / 100;
-            int number10 = (number % 100) / 10;
-            int number1 = number % 10;
-            int strike = hint.get(1);
-            int ball = hint.get(2);
-            int strikeCount = 0;
-            int ballCount = 0;
-            if (a == number100) strikeCount++;
-            if (b == number10) strikeCount++;
-            if (c == number1) strikeCount++;
-
-            if (a == number10 | a == number1) ballCount++;
-            if (b == number100 | b == number1) ballCount++;
-            if (c == number100 | c == number10) ballCount++;
-
-            if (strikeCount == strike & ballCount == ball) count++;
-
-            if (count == N) {
-              answer++;
-              count = 0;
-            }
-          }
-        }
+        arr[i][j] = Integer.parseInt(st.nextToken());
       }
     }
+    recur(0, 100);
+
     System.out.println(answer);
+  }
+
+  private static void recur(int hintIdx, int inputNum) {
+    if (inputNum == 1000) return;
+    if (hintIdx == N) {
+      answer++;
+      recur(0, inputNum + 1);
+      return;
+    }
+    if (checker(hintIdx, inputNum)) {
+      recur(hintIdx + 1, inputNum);
+    } else {
+      recur(0, inputNum + 1);
+    }
+  }
+
+  private static Boolean checker(int hintIdx, int inputNum) {
+    int hintNum = arr[hintIdx][0];
+    int hintStrike = arr[hintIdx][1];
+    int hintBall = arr[hintIdx][2];
+    int hintNum100 = hintNum / 100;
+    int hintNum10 = (hintNum % 100) / 10;
+    int hintNum1 = hintNum % 10;
+    int inputNum100 = inputNum / 100;
+    int inputNum10 = (inputNum % 100) / 10;
+    int inputNum1 = inputNum % 10;
+    int strikeCount = 0;
+    int ballCount = 0;
+    if (inputNum100 == inputNum10 || inputNum10 == inputNum1 || inputNum1 == inputNum100)
+      return false;
+
+    if (inputNum100 == 0 || inputNum10 == 0 || inputNum1 == 0) return false;
+
+    if (inputNum100 == hintNum100) strikeCount += 1;
+    if (inputNum10 == hintNum10) strikeCount += 1;
+    if (inputNum1 == hintNum1) strikeCount += 1;
+
+    if (inputNum100 == hintNum10 || inputNum100 == hintNum1) ballCount += 1;
+
+    if (inputNum10 == hintNum100 || inputNum10 == hintNum1) ballCount += 1;
+    if (inputNum1 == hintNum10 || inputNum1 == hintNum100) ballCount += 1;
+
+    return strikeCount == hintStrike && ballCount == hintBall;
   }
 }
